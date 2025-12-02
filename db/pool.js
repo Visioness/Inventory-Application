c537253240a7f34338c6be6ab16670f220809273
Visioness <aygun-08-zurnaci@hotmail.com>
2025-12-03 02:55:01 +0300

    fix: improve database connection configuration

--- db/pool.js
+++ db/pool.js
@@ -1,16 +1,26 @@
 const { Pool } = require('pg');
 
-const { DB, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD } = process.env;
+const { DATABASE_URL, DB, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD } =
+  process.env;
 
-const pool = new Pool({
-  host: DB_HOST,
-  port: DB_PORT,
-  database: DB,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  ssl: {
-    require: true,
-  },
-});
+const pool = new Pool(
+  DATABASE_URL
+    ? {
+        connectionString: DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        host: DB_HOST,
+        port: DB_PORT,
+        database: DB,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+);
 
 module.exports = pool;
