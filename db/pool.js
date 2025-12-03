e9a0fac0e28cf087e56191bdeebdc19c14a6cb89
Visioness <aygun-08-zurnaci@hotmail.com>
2025-12-03 03:35:05 +0300

    fix: improve SSL configuration for database connections

--- db/pool.js
+++ db/pool.js
@@ -1,26 +1,16 @@
 const { Pool } = require('pg');
 
-const { DATABASE_URL, DB, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD } =
-  process.env;
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
-const pool = new Pool(
-  DATABASE_URL
-    ? {
-        connectionString: DATABASE_URL,
-        ssl: {
-          require: true,
-        },
-      }
-    : {
-        host: DB_HOST,
-        port: DB_PORT,
-        database: DB,
-        user: DB_USER,
-        password: DB_PASSWORD,
-        ssl: {
-          require: true,
-        },
-      }
-);
+const pool = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  user: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
+});
 
 module.exports = pool;
